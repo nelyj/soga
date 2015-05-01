fc366b70efc57e23d47df5a39131541d71102609
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-04-30 23:13:32 -0400

    agrego migracion para user en donde agrego uid y el provider. Estos manejaran facebook dentro del modelo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501024106) do
+ActiveRecord::Schema.define(version: 20150501031205) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 20150501024106) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
