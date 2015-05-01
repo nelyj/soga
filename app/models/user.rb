06a0755ef7bfb62fd1b476e8e0ece8c716aa22fd
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-04-30 23:14:59 -0400

    Agrego un metodo en el modelo usuario que comprueba el usuario de facebook, si existe lo devuelve de lo contrario lo crea

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable, omniauth_providers: [:facebook]
+
+	def self.find_for_facebook_oauth(auth, signed_in_resource=nil)      
+	  user = User.where(provider: auth.provider, uid: auth.uid).first       
+  
+	  return user if user    
+	        
+	  user = User.where(email: auth.info.email).first 
+	  return user if user
+	  User.create(
+	    name: auth.extra.raw_info.name,
+	    provider: auth.provider, uid: auth.uid,
+	    email: auth.info.email,
+	    password: Devise.friendly_token[0,20])  
+	end
 end
