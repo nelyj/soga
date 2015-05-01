0916be7ac4c9407336de57a82f80e39c13cca91d
Felipe Valdivia <fe.valdiviaa@gmail.com>
2015-05-01 01:15:21 -0400

    Add root route for home index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   devise_for :users, 
     controllers: { 
       registrations: "users/registrations", 
@@ -10,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
