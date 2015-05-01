5f595d61d5c4ce23afb0dbc5c623b55f5e142f76
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2015-04-30 23:21:06 -0400

    Agrego la logica para crear un nuevo usuario y redireccionar cuando se conecta con facebook

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,28 +1,23 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
-
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
-
-  # More info at:
-  # https://github.com/plataformatec/devise#omniauth
-
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when omniauth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
+include ApplicationHelper
+  def facebook
+    # Attempt to find the User
+    @user = User.find_for_facebook_oauth(
+      request.env["omniauth.auth"], 
+      current_user
+    )
+    
+    if @user.persisted?
+      sign_in_and_redirect @user, 
+        :event => :authentication 
+      set_flash_message(:notice, 
+        :success, 
+        :kind => "Facebook") if is_navigational_format?
+    else
+      redirect_to new_user_registration_url
+    end
+  end
+  def passthru
+    render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+  end
 end
